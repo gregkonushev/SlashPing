03468aaaa77584ec5516542b191ff75fe2c6bb77
Grigory Konushev <greg@narvar.com>
2017-02-17 16:57:01 -0800

    get instead of post with query params

--- app/controllers/Commands.scala
+++ app/controllers/Commands.scala
@@ -1,14 +1,12 @@
 package controllers
 
+import play.api.data.{Form, Forms}
 import play.api.mvc.{Action, Controller}
 
-/**
-  * Created by Grigory Konushev on 2/17/17.
-  */
 object Commands extends Controller {
 
   def ping = Action { Request =>
-    println(Request.body)
+    println(Request.queryString get "team_domain")
     Ok("I think your table is available right now")
   }
 }
