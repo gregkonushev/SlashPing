3ad6880a23364cec6fa227b3702a9b68f6dabb95
Grigory Konushev <greg@narvar.com>
2017-02-17 16:11:41 -0800

    print request body back

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,8 +8,9 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
-  def ping = Action {
-    Ok("I think you table is available right now")
+  def ping = Action { Request =>
+    Ok(s"I think you table is available right now ${Request.body.asText}")
+
   }
 
 }
